3773e70f0d0674d980d1d9ed8e699cbde1dacad5
ItC <ivan120536@gmail.com>
2022-02-07 19:13:18 +0400

    fix2

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ TEMPLATE_PATH = 'templates'
 
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/index/<title>')
+def index(title='Main_page'):
     params = {
-        'title': 'Main page',
+        'title': title,
         'navbar_title': 'Миссия Колонизация Марса '
     }
     return render_template('index.html', **params)
@@ -18,8 +18,13 @@ def index():
 
 @app.route('/training/')
 @app.route('/training/<prof>')
-def training(prof):
-    pass
+def training(prof="врач".lower()):
+    params = {
+        'title': 'Main page',
+        'navbar_title': 'Миссия Колонизация Марса',
+        'prof': prof
+    }
+    return render_template('training.html', **params)
 
 
 if __name__ == '__main__':
